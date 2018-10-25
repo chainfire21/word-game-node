b0345a69914b7bce9eee1ba7f5a189c915f9c9cc
paredfed <paredfed@gmail.com>
2018-10-24 19:17:00 -0500

    all done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Word = require("./word.js");
 const inquirer = require("inquirer");
-const words = ["hallucination", "blah", "full stack web developer", "triangle", "computer", "dihidrogen monoxide"];
+const words = ["hallucination", "blah", "full stack", "web developer", "triangle", "computer", "dihidrogen monoxide", "trick or treat"];
 let guesses = 6;
 let toGuess = new Word(words[Math.floor(Math.random() * words.length)]);
+const guessedLetters = [];
+let alreadyGuessed = false;
 console.log("");
 console.log(toGuess.generateWord());
 console.log("\nYou start with 6 guesses!");
@@ -13,37 +15,72 @@ function game() {
             type: "input",
             name: "letterGuess",
             message: "Guess a letter!",
+            validate: function (value) {
+                var pass = value.match(
+                    /^.$/
+                );
+                if (pass) {
+                    return true;
+                }
+
+                return 'Please enter a single character';
+            }
         },
     ]).then((answer) => {
+        guessedLetters.forEach(letter => {
+            if (letter.toLowerCase() === answer.letterGuess.toLowerCase()) {
+                alreadyGuessed = true;
+            }
+        });
+        if (alreadyGuessed) {
+            console.log("You already guessed that letter!");
+            alreadyGuessed = false;
+            return game();
+        }
         if (toGuess.guess(answer.letterGuess)) {
             console.log("\nCorrect!");
+            updateGuessed(answer.letterGuess);
         }
         else {
             guesses--;
-            console.log(`\nYou have ${guesses} left!`);
+            console.log(`\nIncorrect!!! You have ${guesses} guesses left!`);
+            updateGuessed(answer.letterGuess);
         }
         console.log("");
         console.log(toGuess.generateWord());
+        if (guesses === 0) {
+            console.log("You lost!!");
+            return resetGame();
+
+        }
         if (toGuess.checkWin()) {
             console.log("You win!");
-            inquirer.prompt([{
-                type: "confirm",
-                name: "restartGame",
-                message: "Play again?",
-            },
-            ]).then(ans => {
-                if (ans.restartGame) {
-                    guesses = 6;
-                    toGuess = new Word(words[Math.floor(Math.random() * words.length)]);
-                    console.log(toGuess.generateWord());
-                    return game();
-                }
-                return console.log("Goodbye!");
-            });
+            return resetGame();
         }
         else {
             game();
         }
     });
 };
+const updateGuessed = function (letter) {
+    guessedLetters.push(letter);
+    console.log(`\nLetters guessed: ${guessedLetters.join(", ")}`);
+}
+const resetGame = function () {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "restartGame",
+        message: "Play again?",
+    },
+    ]).then(ans => {
+        if (ans.restartGame) {
+            guesses = 6;
+            guessedLetters.length = 0;
+            toGuess = new Word(words[Math.floor(Math.random() * words.length)]);
+            console.log(toGuess.generateWord());
+            return game();
+        }
+        return console.log("Goodbye!");
+    });
+}
 game();
