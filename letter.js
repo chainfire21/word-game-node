dd7822f727399811b1b95ce0592df239418e26a1
paredfed <paredfed@gmail.com>
2018-10-26 16:39:58 -0500

    using prototype

--- letter.js
+++ letter.js
@@ -1,23 +1,22 @@
-function Letter(letter){
+function Letter(letter) {
     this.letter = letter;
     this.guessed = false;
-    this.character = function(){
-        if (this.guessed) {
-            return this.letter;
-        }
-        else  if (this.letter ===" "){ 
-            this.guessed = true;
-            return " ";
-        }
-        else return " _";
+}
+Letter.prototype.character = function () {
+    if (this.guessed) {
+        return this.letter;
     }
-    this.check = function(char){
-        if (char.toLowerCase() === letter.toLowerCase()){
-            this.guessed = true;
-            return true;
-        }
-        return false;
+    else if (this.letter === " ") {
+        this.guessed = true;
+        return " ";
     }
-
+    else return " _";
+}
+Letter.prototype.check = function (char) {
+    if (char.toLowerCase() === letter.toLowerCase()) {
+        this.guessed = true;
+        return true;
+    }
+    return false;
 }
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,33 +1,34 @@
 const Letter = require("./letter.js")
-function Word(word){
+function Word(word) {
     this.word = word;
-    this.letterArr = word.split('').map((x)=>{
-        return new Letter(x);
-    });
-    this.generateWord = function(){
-        let displayWord = "";
-        this.letterArr.forEach(char =>{         
-            displayWord += char.character();
-        });
-        return displayWord;
-    }
-    this.guess = function(guess){
-        let correctGuess = false;
-        this.letterArr.forEach((char)=>{
-            if(char.check(guess)){
-                correctGuess = true;
-            }
-        });
-        return correctGuess;
-    }
-    this.checkWin = function(){
-        let checkWinStatus = true;
-        this.letterArr.forEach(char=>{
-            if (!char.guessed){
-                checkWinStatus = false;
-            }
-        });
-        return checkWinStatus;
-    }
 }
+Word.prototype.letterArr = word.split('').map((x) => {
+    return new Letter(x);
+});
+Word.prototype.generateWord = function () {
+    let displayWord = "";
+    this.letterArr.forEach(char => {
+        displayWord += char.character();
+    });
+    return displayWord;
+}
+Word.prototype.guess = function (guess) {
+    let correctGuess = false;
+    this.letterArr.forEach((char) => {
+        if (char.check(guess)) {
+            correctGuess = true;
+        }
+    });
+    return correctGuess;
+}
+this.checkWin = function () {
+    let checkWinStatus = true;
+    this.letterArr.forEach(char => {
+        if (!char.guessed) {
+            checkWinStatus = false;
+        }
+    });
+    return checkWinStatus;
+}
+
 module.exports = Word;
